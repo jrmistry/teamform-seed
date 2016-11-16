928a037952816a3a53188882153f028601e33b40
Jaydeep <jd.mistry@gmail.com>
2016-11-16 13:35:14 +0800

    completing memberJS testing

--- app/unit_tests/test_member.js
+++ app/unit_tests/test_member.js
@@ -5,7 +5,9 @@ var firebase = {
 };
 
 describe('Test member.js', function () {
-    var firebaseParam = {
+    var paramFunction, catchFunction;
+
+    var firebaseParam1 = {
         $loaded: function () {
             return {
                 then: function (func) {
@@ -16,18 +18,41 @@ describe('Test member.js', function () {
                     }
                 }
             };
+        },
+        $save: jasmine.createSpy(),
+        'memberName':'Member11'
+    };
+
+    var firebaseParam2 = {
+        $loaded: function () {
+            return {
+                then: function (func) {
+                    paramFunction = func;
+
+                    return {
+                        catch: function (func) {
+                            catchFunction = func;
+                            return [];
+                        }
+                    }
+                }
+            };
         }
     };
 
-    var $firebaseObject = function () {
-        return firebaseParam;
+    var $firebaseObject1 = function () {
+        return firebaseParam1;
+    };
+
+    var $firebaseObject2 = function () {
+        return firebaseParam2;
     };
 
     var $firebaseArray = function () {
         return [];
     };
 
-    var $rootScope, $state, $controller;
+    var $rootScope, $state, $controller, $save, testCallback, testStateGo;
 
     beforeEach(module('teamform'));
 
@@ -36,7 +61,13 @@ describe('Test member.js', function () {
         $rootScope = _$rootScope_;
         $controller = _$controller_;
 
-        createController = function () {
+        $rootScope.retrieveOnceFirebase = function(firebase, path, callback) {
+            testCallback = callback;
+        }
+
+        spyOn($state, 'go');
+
+        createController = function ($firebaseObject, $firebaseArray) {
             return $controller('MemberCtrl', {
                 '$scope': $rootScope,
                 '$firebaseObject': $firebaseObject,
@@ -48,15 +79,116 @@ describe('Test member.js', function () {
     }));
 
     it('test initial value of userID', function () {
-        var controller = createController();
+        createController($firebaseObject2, $firebaseArray);
         expect($rootScope.userID).toBe('');
     });
     it('test initial value of userName', function () {
-        var controller = createController();
+        createController($firebaseObject2, $firebaseArray);
         expect($rootScope.userName).toBe('');
     });
     it('test initial value of teams', function () {
-        var controller = createController();
+        createController($firebaseObject2, $firebaseArray);
         expect($rootScope.teams.length).toBe($firebaseArray().length);
+    });
+
+    it('test loadFunc success', function () {
+        createController($firebaseObject2, $firebaseArray);
+
+        $rootScope.userID = 'randomID';
+
+        $rootScope.loadFunc();
+
+        var testName = "testName";
+        var testSelection = ["team1", "team2"];
+
+        var dummyData = {
+            memberName: "testName",
+            selection: ["team1", "team2"],
+            child: function(name) {
+                return {
+                    property: this[name],
+                    val: function() {
+                        return (this.property);
+                    }
+                }
+            }
+        };
+
+        testCallback(dummyData);
+        expect($rootScope.userName).toBe(testName);
+        expect($rootScope.selection.length).toBe(testSelection.length);
+        expect($rootScope.selection[0]).toBe('team1');
+    });
+    it('test loadFunc bad userID', function () {
+        createController($firebaseObject2, $firebaseArray);
+
+        $rootScope.userID = '';
+
+        $rootScope.loadFunc();
+
+        expect($rootScope.userName).toBe('');
+        expect($rootScope.selection.length).toBe(0);
+    });
+    it('test loadFunc null data', function () {
+        createController($firebaseObject2, $firebaseArray);
+
+        $rootScope.userID = 'randomID';
+
+        $rootScope.loadFunc();
+
+        var testName = "testNameFail";
+
+        var dummyData = {
+            memberName: null,
+            selection: null,
+            child: function(name) {
+                return {
+                    property: this[name],
+                    val: function() {
+                        console.log(this);
+                        return (this.property);
+                    }
+                }
+            }
+        };
+
+        testCallback(dummyData);
+        expect($rootScope.userName).toBe('');
+        expect($rootScope.selection.length).toBe(0);
+    });
+
+    it('test saveFunc success', function () {
+        createController($firebaseObject1, $firebaseArray);
+        $rootScope.$state = jasmine.createSpy();
+
+        $rootScope.userID = 'Member99';
+        $rootScope.userName = 'Member 99';
+
+        $rootScope.saveFunc();
+        expect($rootScope.userID).toBe('Member99');
+        expect($rootScope.userName).toBe('Member 99');
+    });
+    it('test saveFunc fail no name', function () {
+        createController($firebaseObject1, $firebaseArray);
+        $rootScope.$state = jasmine.createSpy();
+
+        $rootScope.userID = '';
+        $rootScope.userName = '';
+
+        $rootScope.saveFunc();
+
+        expect($rootScope.userID).toBe('');
+        expect($rootScope.userName).toBe('');
+    });
+
+    it('test refreshTeams', function () {
+        createController($firebaseObject1, $firebaseArray);
+        $rootScope.$state = jasmine.createSpy();
+
+        $rootScope.userID = 'Member99';
+        $rootScope.userName = 'Member 99';
+
+        $rootScope.refreshTeams();
+        expect($rootScope.userID).toBe($rootScope.userID);
     });
 });
