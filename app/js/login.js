0029462eb4a9b7315a395569602c3ad917de0ec4
Jaydeep <jd.mistry@gmail.com>
2016-11-29 17:06:10 +0800

    Adding specific login button

--- app/js/login.js
+++ app/js/login.js
@@ -1,19 +1,22 @@
 angular.module('teamform')
-        .controller(
-        'LoginCtrl',
-        ['$scope', '$firebaseObject', '$firebaseArray', '$state',
-            function($scope, $firebaseObject, $firebaseArray, $state,asfdasd) {
-                $scope.goToAdmin = function() {
-                    $state.go("events", {event: $scope.event});
-                };
+    .controller('LoginCtrl', ['$scope', '$state', 'Models',
+        function ($scope, $state, models) {
+            $scope.loginID = "Admin22";
 
-                $scope.goToTeam = function() {
-                    $state.go("team_creation", {event: $scope.event});
-                };
+            $scope.goToAdmin = function () {
+                $state.go("events", { event: $scope.event });
+            };
 
-                $scope.goToMember = function() {
-                    $state.go("member", {event: $scope.event});
-                };
-            }
-        ]
-);
+            $scope.goToTeam = function () {
+                $state.go("team_creation", { event: $scope.event });
+            };
+
+            $scope.goToMember = function () {
+                $state.go("member", { event: $scope.event });
+            };
+            $scope.login = function () {
+                
+            };
+        }
+    ]
+    );
