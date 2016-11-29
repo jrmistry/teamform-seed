5a28855b87c9921e063112268c00f3f30945f078
Vishal Bollu <vbollu@uwaterloo.ca>
2016-11-29 23:08:23 +0800

    yolo

--- app/js/member.js
+++ app/js/member.js
@@ -7,37 +7,30 @@ angular.module('teamform')
             $scope.eventID = $stateParams.event;
             $scope.memberID = $stateParams.memberID;
             $scope.teamList = [];
+            $scope.inTeam = true;
+
             models.getEvent($scope.eventID).$loaded(function(event){
                 $scope.event = event;
+                if (event.teams == undefined) {
+                    $scope.teams = [];
+                    event.teams = [];
+                }
                 $scope.teams = event.teams;
+                $scope.member = event.members[$scope.memberID];
+                $scope.inTeam = $scope.member.teamID != undefined;
 
                 for (var index in $scope.teams) {
                     $scope.teams[index].id = index;
-                    $scope.teamList.push($scope.teams[index]);
+                    if (!$scope.inTeam || $scope.member.teamID == ) {
+                        $scope.teamList.push($scope.teams[index]);
+                    }
                 }
                 $scope.teams = event.teams;
-                $scope.member = event.members[$scope.memberID]; 
             });
 
             $scope.search = function() {
                 $scope.teamList = search.forTeams($scope.event, $scope.searchTerm);
             };
-
-            $scope.requestForTeam = function(teamID) {
-                if ($scope.member.invites == undefined) {
-                    $scope.member.invites = [];
-                }
-                $scope.member.invites.push(teamID);
-
-                var team = $scope.teams[teamID];
-
-                if (team.requests == undefined) {
-                    team.requests = [];
-                }
-                team.requests.push($scope.memberID);
-                $scope.event.$save();
-            };
-            
 
             $scope.requestForTeam = function(teamID) {
                 if ($scope.member.invites == undefined) {
