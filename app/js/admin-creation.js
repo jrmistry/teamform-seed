c95343aef29d7ee28749a5d3aff7b80131245eb3
mallika <mtrivedi@uwaterloo.ca>
2016-11-29 17:42:38 +0800

    completed event creation

--- app/js/admin-creation.js
+++ app/js/admin-creation.js
@@ -1,8 +1,12 @@
 angular.module('teamform')
-    .controller('AdminCreationCtrl', ['$scope', '$stateParams', '$state',
-        function($scope, $stateParams, $state) {
+    .controller('AdminCreationCtrl', ['$scope', '$firebaseObject', '$firebaseArray', '$stateParams', '$state', 'Models',
+        function ($scope, $firebaseObject, $firebaseArray, $stateParams, $state, models) {
 
-            // Initialize $scope.param as an empty JSON object
+            $scope.adminName = "";
+            $scope.eventID = "";
+            $scope.eventName = "";
+            $scope.eventDesc = "";
+
             $scope.param = {};
             $scope.param.maxTeamSize = 10;
             $scope.param.minTeamSize = 1;
@@ -21,4 +25,22 @@ angular.module('teamform')
                 }
             };
 
+            $scope.createEvent = function() {
+                if ($scope.adminName != "" && $scope.eventID != "" && $scope.eventName != "" && $scope.eventDesc != "") {
+                    var newData = {
+                        'admin': {'name': $scope.adminName},
+                        'desc':$scope.eventDesc,
+                        'maxTeamSize': $scope.param.maxTeamSize,
+                        'members':{},
+                        'minTeamSize': $scope.param.minTeamSize,
+                        'name': $scope.eventName,
+                        'teams':{},
+                    };
+
+                    var events = models.getAllEvents();
+                    events.$loaded().then(function () {
+                        events.$ref().child($scope.eventID).set(newData);
+                    });
+                }
+            }
         }]);
