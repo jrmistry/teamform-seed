f9fa86171fb3a411c70e83f09d1849c721650a12
Vishal Bollu <vbollu@uwaterloo.ca>
2016-11-29 18:02:07 +0800

    search for members

--- app/js/search-service.js
+++ app/js/search-service.js
@@ -42,6 +42,27 @@
                     });
 
                     return result;
-                }
+                };
+
+                this.forMembers = function(event, text) {
+                    var resultList = [];
+                    console.log(Object.keys(event.members));
+                    Object.keys(event.members).forEach(function(key){
+                        var shouldAdd = false;
+                        var member = event.members[key];
+                        if (member.name.indexOf(text) > -1) {
+                            shouldAdd = true;
+                        }
+
+                        member.skills.forEach(function(skill){
+                            if (skill == text) {shouldAdd = true;}
+                        });
+
+                        if (shouldAdd) {
+                            resultList.push(member);
+                        }
+                    });
+
+                };
             }]);
 }(angular));
