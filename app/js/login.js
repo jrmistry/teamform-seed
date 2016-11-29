8aa85e0690b761071425315788ed3a5695ea21ba
Jaydeep <jd.mistry@gmail.com>
2016-11-29 20:45:08 +0800

    Hotfix to login into adminHTML

--- app/js/login.js
+++ app/js/login.js
@@ -26,7 +26,7 @@ angular.module('teamform')
                                 console.log("Invalid Login ID");
                             } else if (result.type == "admin") {
                                 $scope.eventID = result.event;
-                                $scope.goToAdmin();
+                                $state.go("events", {eventID: $scope.eventID, memberID: result.member});
                             } else if (result.type == "member") {
                                 $scope.eventID = result.event;
                                 $state.go("member", {event: $scope.eventID, member: result.member});
