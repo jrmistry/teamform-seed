d373d36ccd4f3f25f2bbf790047b69886fd13884
Vishal Bollu <vbollu@uwaterloo.ca>
2016-11-29 23:22:39 +0800

    fixing member so that it doesnt show request when member is already part of the thing

--- app/js/member.js
+++ app/js/member.js
@@ -9,7 +9,7 @@ angular.module('teamform')
             $scope.teamList = [];
             $scope.inTeam = true;
 
-            models.getEvent($scope.eventID).$loaded(function(event){
+            $scope.load = function(event){
                 $scope.event = event;
                 if (event.teams == undefined) {
                     $scope.teams = [];
@@ -26,24 +26,20 @@ angular.module('teamform')
                     }
                 }
                 $scope.teams = event.teams;
-            });
+            };
+
+            models.getEvent($scope.eventID).$loaded($scope.load);
 
             $scope.search = function() {
                 $scope.teamList = search.forTeams($scope.event, $scope.searchTerm);
             };
 
             $scope.requestForTeam = function(teamID) {
-                if ($scope.member.invites == undefined) {
-                    $scope.member.invites = [];
+                $scope.member.teamID = teamID;
+                if ($scope.event.teams[teamID].members == undefined) {
+                    $scope.event.teams[teamID].members = [];
                 }
-                $scope.member.invites.push(teamID);
 
-                var team = $scope.teams[teamID];
-
-                if (team.requests == undefined) {
-                    team.requests = [];
-                }
-                team.requests.push($scope.memberID);
-                $scope.event.$save();
+                $scope.event.teams.members.push($scope.memberID);
             };
 }]);
